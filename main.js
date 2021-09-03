ab5038c376e26838d0c5d2d29fba91465b1ddcca
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-02 21:45:10 -0500

    uso de map y reduce

--- main.js
+++ main.js
@@ -148,3 +148,110 @@ const alumnos = [];
 // ejercicio 2: solicitar al usuario 10 nombres
 // regresar los nombres con longitud par
 // o que tengan la cuarta letra del nombre sea vocal
+
+// map: regresa un nuevo arreglo de todos los elemntos que tenga el
+// arreglo principalq
+
+// ejemplo 
+
+const personas = [
+    { nombre: 'Maria', apellidoPaterno: 'Perez', apellidoMaterno: 'Sanchez' },
+    { nombre: 'Antonio', apellidoPaterno: 'Gonzalez', apellidoMaterno: 'Juarez' },
+    { nombre: 'Saúl', apellidoPaterno: 'Martinez', apellidoMaterno: 'Perez' },
+    { nombre: 'Ernesto', apellidoPaterno: 'Ramirez', apellidoMaterno: 'Martinez' },
+    { nombre: 'Alan', apellidoPaterno: 'Maldonado', apellidoMaterno: 'Gonzalez' },
+];
+
+// obtener solo los apellidos de todos los elementos
+
+const apellidos = personas.map( 
+    (valor, index, array) => `${valor.apellidoPaterno} ${valor.apellidoMaterno}`
+); // ['Perez Sanchez', 'Gonzalez Juarez', 'Martinez Perez', 'Ramirez Martinez', 'Maldonado Gonzalez']
+
+
+// ejemplo
+
+const numerosx = [1,5,7,9,90,123,456];
+
+// obtener una nuevo arreglo con la multiplicacion de los elementos por 3
+
+const multipor3 = numerosx.map( valor => valor * 3); 
+// [3, 15, 21, 27, 270, 369, 918]
+
+// obtener una nuevo arreglo con la multiplicacion de los elementos por 3
+// pero solo los pares
+
+const multipares = numerosx.map( valor => {
+    if (valor % 2 === 0) {
+        return valor * 3;
+    }
+    // return valor;
+});
+
+// ejercicio 3: de un grupo de 6 alumnos, obtener si reprueba o aprueba 
+// dando la calificacion de 5 materias
+// si el promedio del alumno es menor a 7 'reprobado'
+// si el promedio del alumno es mayoe a 7 pero menor a 10 'aceptable'
+// si el promedio del alumno es igual a 10 'excelente'
+
+//  entregar promedio y descripcion dependiendo las reglas antes mencionadas
+
+
+// reduce: me permite regresar cualquier tipo de dato,
+// es tambien conocido como fold
+
+// estructura
+
+const numerosxx = [1,2,3,4,5,6,7,8,9]; //45
+
+// obtener la suma de todos los numeros
+
+const sumaxx = numerosxx.reduce( (valor_previo,value, index, array) => {
+    return valor_previo + value;
+});
+
+// pruebas de escritorio
+
+// iteracion 0 cuando tenemos el segundo paramertro del reduce
+// valor_previo = 0
+// value = posicion 0 de mi arreglo
+
+// iteracion 0 cuando no ponemos el segundo parametro del reduce
+// valor_previo = posicion 0 de mi arreglo
+// value= posicion 1 de mi arreglo
+
+// iteracion 1 en adelante
+// valor_previo = 3 (return)
+// value = 3
+
+const alumnosxy = [
+    {inscrito: true, materia1: 10, materia2: 10, materia3: 10, materia4: 10, materia5: 10, nombre: 'Tom'},
+    {inscrito: true, materia1: 8, materia2: 7, materia3: 9, materia4: 6, materia5: 9, nombre: 'John'},
+    {inscrito: true, materia1: 9, materia2: 10, materia3: 8, materia4: 8, materia5: 9, nombre: 'Ana'},
+    {inscrito: true, materia1: 8, materia2: 8, materia3: 5, materia4: 10, materia5: 8, nombre: 'Laura'},
+    {inscrito: true, materia1: 5, materia2: 9, materia3: 10, materia4: 7, materia5: 9, nombre: 'Peter'},
+    {inscrito: true, materia1: 7, materia3: 3, materia4: 5, materia5: 10, nombre: 'Antonio'}
+];
+
+const promedios = alumnosxy.reduce((acc, value, index) => {
+    const prom = Object.keys(value).reduce((sum, mat) => { 
+        // ['inscritos', 'materia1', 'materia2', 'materia3', 'materia4', 'materia5', 'nombre]
+        if (mat.includes('materia')) {
+            return sum += value[mat]; // sum = sum + value[mat];
+        }
+        return sum;
+    }, 0) / 5;
+    acc['alumno'+(index+1)] = {
+        promedio: prom,
+        nombre: value.nombre,
+        descripcion: prom < 7
+            ? 'Reprobado'
+            : prom >= 7 && prom < 10 
+                ? 'Aceptable'
+                : 'Excelente'
+    };
+    return acc;
+}, {});
+
+console.log('promedios: ', promedios);
+
